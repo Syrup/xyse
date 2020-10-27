13a8a2d1240e7907689e33b83f6a7c9df56361bb
Fabian maulana <54355288+Miouns@users.noreply.github.com>
2020-10-27 11:57:43 +0700

    update xyse.js

--- xyse.js
+++ xyse.js
@@ -1,16 +1,6 @@
 const Discord = require("discord.js");
 const Xyve = require("./handler/Xyve.js"); 
 const client = new Xyve();
-const axios = require("axios")
-const urls = ["https://xyse-disc.glitch.me/"]
-      let channel = client.channels.cache.get('748516588399099975');
-
-setInterval(function() {
-            urls.forEach(url => {
-                          axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
-
-        })
-    }, 60 * 1000);
 require("./handler/module.js")(client);
 require("./handler/Event.js")(client);
 
